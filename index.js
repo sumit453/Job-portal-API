2160c050d5dd87a047d72f4b264abf84f0b4f3bd
Sumit Kumar Das <sumitdas386@gmail.com>
2025-09-10 14:29:25 +0530

    This is the 2nd commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "./env.js";
 import express from "express";
 import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 import connectUsingMongoose from "./src/config/mongoose.config.js";
 import userRouts from "./src/features/users/user.routes.js";
@@ -8,11 +9,21 @@ import jobRoutes from "./src/features/jobs/jobs.routes.js";
 import jwtAuth from "./src/middleware/jwtAuth.middleware.js";
 import errorHandlingMiddleware from "./src/middleware/errorHandling.middleware.js";
 import applicationRoutes from "./src/features/application/application.routes.js";
+import loggerMiddleware from "./src/middleware/logs/logs.middleware.js";
 
 const server = express();
 
 server.use(helmet());
+
+server.use(
+  rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100,
+  })
+);
+
 server.use(express.json());
+server.use(loggerMiddleware);
 
 server.use("/api/users", userRouts);
 server.use("/api/jobs", jwtAuth, jobRoutes);
